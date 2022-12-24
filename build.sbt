fd7dc17a11749a2f484a722f1529b232877ed4df
David Albacete <david.albacete@acconeer.com>
2022-12-24 18:00:34 +0100

    second commit

--- build.sbt
+++
@@ -1,7 +0,0 @@
-organization := "edu.berkeley.cs"
-
-version := "1.0"
-
-name := "SmallBoomAndRocketConfig"
-
-scalaVersion := "2.12.4"
--- src/main/scala/example.scala
+++
@@ -1,9 +0,0 @@
-class SmallBoomAndRocketConfig extends Config(
-	new WithTop ++
-	new WithBootROM ++
-	new freechips.rocketchip.subsystem.WithInclusiveCache ++
-	new boom.common.WithRenumberHarts ++
-	new boom.common.WithSmallBooms ++
-	new boom.common.WithNBoomCores(1) ++
-	new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-	new freechips.rocketchip.system.BaseConfig)
